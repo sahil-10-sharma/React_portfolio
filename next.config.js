dca31b45ce6e45807875dc1522e8b543675239c6
Sahil Sharma <sahil.catalan10@gmail.com>
2021-12-06 12:46:05 +0530

    f

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = withImages()
 
 module.exports = {
     env: {
-      "DEV_API_URL": "http://localhost:5000",
+      "DEV_API_URL": "https://portfolio-server34.herokuapp.com/",
       "PRODUCTION_API_URL": "https://www.api.example.com"
     }
   }
